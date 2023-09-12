f10347eaeda77dbf7b541914fa8cfdd22e5b617c
jtoney40 <jtoney40@charlottesvilleschools.org>
2023-09-12 11:41:02 -0400

    distancesor code added

--- servocode.py
+++ servocode.py
@@ -13,13 +13,13 @@ from adafruit_motor import servo
 # create a PWMOut object on Pin A2.
 pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 
-btn1 = DigitalInOut(board.D2)
+btn1 = DigitalInOut(board.D5)
 btn1.direction = Direction.INPUT
 btn1.pull = Pull.DOWN
 
 prev_state = btn1.value
 
-btn2 = DigitalInOut(board.D3)
+btn2 = DigitalInOut(board.D2)
 btn2.direction = Direction.INPUT
 btn2.pull = Pull.DOWN
 
@@ -28,14 +28,14 @@ prev_state = btn2.value
 my_servo = servo.Servo(pwm)
 
 while True:
-   if btn2.value:
+    if btn1.value:
         print("BTN1  is down")  
         for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
             my_servo.angle = angle
-        time.sleep(0.05) 
-        if btn2.value:
-            print("BTN2  is down") 
+      
+    if btn2.value:
+        print("BTN2  is down") 
         for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
             my_servo.angle = angle
-        time.sleep(0.05)
-        
+    
+    time.sleep(0.35) 
