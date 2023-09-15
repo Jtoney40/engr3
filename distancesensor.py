7290aecd37b3c521c4be96d105467da816db5bde
jtoney40 <jtoney40@charlottesvilleschools.org>
2023-09-15 10:55:31 -0400

    fixed problem

--- distancesensor.py
+++ distancesensor.py
@@ -1,7 +1,6 @@
 import time
 import board
 import adafruit_hcsr04
-
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 
 
@@ -11,3 +10,4 @@ while True:
     except RuntimeError:
         print("Retrying!")
     time.sleep(0.1)
+    
